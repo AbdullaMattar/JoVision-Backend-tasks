41f0d058e167615da1d34018c07cf8ab76708a1a
M3code <abdullamatter3@gmail.com>
2026-02-19 15:50:12 +0300

    task44

---
+++ Controllers/Greeter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoVision_Backend_tasks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreeterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Greet([FromQuery] string? name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                name = "anonymous";
+           
+            return Ok($"Hello {name}");
+        }
+    }
+}
